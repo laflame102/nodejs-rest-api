4883d1746ff14103c732b4a0ad316003b7e45716
laflame102 <109043511+laflame102@users.noreply.github.com>
2023-06-01 16:26:28 +0300

    added jimp in updateAvatar method(auth.js), set size for avatars

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 const gravatar = require("gravatar");
 const path = require("path");
 const fs = require("fs/promises");
+const Jimp = require("jimp");
 
 const { User } = require("../models/user");
 const { HttpError } = require("../helpers");
@@ -103,6 +104,8 @@ const updateAvatar = async (req, res) => {
   const filename = `${_id}_${originalname}`;
   const resultUpload = path.join(avatarsDir, filename);
   await fs.rename(tempUpload, resultUpload);
+  const resizeAvatar = await Jimp.read(resultUpload);
+  await resizeAvatar.resize(250, 250).write(resultUpload);
   const avatarURL = path.join("avatars", filename);
 
   await User.findByIdAndUpdate(_id, { avatarURL });
